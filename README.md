253a79af7a131cd2fd9e13a7dd702dab4f098dcb
brandogn <bwong928@berkeley.edu>
2022-11-04 21:06:37 -0700

    initialized git repo

---
+++ README.md
@@ -0,0 +1,5 @@
+## TODO
+
+ [ ] finish doc page templates and parsing
+ [ ] maybe finish bash scripting to automatically add each non home page to the list of homes idk
+ [ ] Finish adding all the stuff from old machina
---
+++ build_pugs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Store Templates in vars
+
+ROOT="$(pwd)"
+HOME_DIR="${ROOT}/templates/template_folder/template_home.pug"
+PAGE_DIR="${ROOT}/templates/template_folder/template_page.pug"
+
+# Create file structure
+
+echo "Creating structure in ${ROOT}"
+mkdir pages/; cd pages/
+
+for i in "I" "II" "III"
+do
+    mkdir "Proj_${i}"
+    cp $HOME_DIR "Proj_${i}/p_${i}_home.pug"
+done
+
+
+# pug render
+
+cd ..
+echo "pug output:"
+pug ./ -o ./docs -P
+
+
+# Option to remove /pages dir, mainly for debugging
+
+echo "Remove Directory? [y/n]"
+read rm_input
+
+if [ rm_input == "y"]
+then
+    rm -rf pages
+fi
